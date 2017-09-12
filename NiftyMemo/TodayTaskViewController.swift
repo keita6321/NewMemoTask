b4adfa1011751ca8804ee8fe486540811c9f469a
nttr <nttr@BI-23559.local>
2017-09-12 14:47:46 +0900

    make Memo & Task interface add some type task windows

--- NiftyMemo/TodayTaskViewController.swift
+++ NiftyMemo/TodayTaskViewController.swift
@@ -1,5 +1,5 @@
 //
-//  MemoViewController.swift
+//  ExpiredTaskViewController.swift
 //  NiftyMemo
 //
 //  Created by nttr on 2017/09/12.
@@ -7,43 +7,60 @@
 //
 
 import UIKit
-import NCMB
 import SVProgressHUD
+import NCMB
 
-class MemoViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+class TodayTaskViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
     @IBOutlet var memoTableView: UITableView!
     var memoArray = [NCMBObject]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        memoTableView.delegate = self
         memoTableView.dataSource = self
-        //loadMemo()
+        memoTableView.delegate = self
+        // Do any additional setup after loading the view.
     }
-
+    override func viewWillAppear(_ animated: Bool) {
+        loadTodayMemo()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    override func viewWillAppear(_ animated: Bool) {
-        loadMemo()
-    }
     
     //メモの個数を返す
+    //func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    //    return memoArray.count
+    //}
+    //セルを返す
+    //func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    //    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+    // cell内のcontentViewの背景を透過
+    //    cell.backgroundColor = UIColor.clear
+    //    cell.contentView.backgroundColor = UIColor.clear
+    //    cell.textLabel?.text = memoArray[indexPath.row].object(forKey: "text") as! String
+    //    return cell
+    //}
+    
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return memoArray.count
     }
-    //セルを返す
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
         cell.textLabel?.text = memoArray[indexPath.row].object(forKey: "text") as! String
         return cell
     }
-
-
-    func loadMemo(){
-        let query = NCMBQuery(className: "Memo")
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "toDetail", sender: nil)
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func loadTodayMemo(){
+        let query = NCMBQuery(className: "NiftyMemo")
+        query?.whereKey("today", equalTo: true)
         query?.findObjectsInBackground({ (result, error) in
             if error != nil{
                 SVProgressHUD.showError(withStatus: error?.localizedDescription)
@@ -54,5 +71,5 @@ class MemoViewController: UIViewController, UITableViewDelegate, UITableViewData
             }
         })
     }
-
+    
 }
